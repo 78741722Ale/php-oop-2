4c90ab9b0f31c4481c2d601765a542d2ed9963e5
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-26 15:11:02 +0200

    Analizzato la consegna nel file index.php. Iniziato struttura dati

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php 
-/* Var Dump Iniziale */
-var_dump('Prova Collegamento Htdocs');
+
+/* 
+Consegna : 
+L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+
+- Products genitore
+- Product (cibo, giochi , cucce)
+
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e 
+ricevere il 20% di sconto su tutti i prodotti.
+
+- Users (classe Genitore)
+- User-registrato (classe figlia | Martina, ALessandro, Andrea, Fabiola Ecc..) (ottiene il 20% di sconto)
+- user-non-registrato (non ottiene il 20% di sconto)
+
+Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+carta di credito appartiene a user singolo
+*/
 
 ?>
