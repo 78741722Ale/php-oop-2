94e1c670453a131b60437583ea116e27db4d638b
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-26 17:12:23 +0200

    Io lo pusho cosi, devo studiare non ho capito

--- index.php
+++ index.php
@@ -28,4 +28,16 @@ Il pagamento avviene con la carta di credito, che non deve essere scaduta.
 carta di credito appartiene a user singolo
 */
 
+/* Provo a verificare Utente */
+
+$users = [
+    new User('Alessandro', 'Pecorilla', 'Leonardo Da Vinci N°5', 'Prova@prova.com', 'Bancomat', 555, 27, 'prova'),
+    new User('Fabiola', 'Pacifici', 'Leonardo Da Vinci N°15', 'Prova2@prova2.com', 'Contanti', 556, 33, 'prova2'),
+];
+
+var_dump("Questa è la verifica dell'array di classi User");
+var_dump($users);
+
+$prodotto = new Products('Cariola', 'Strumento', 'E uno strumento', 55 , 'Non lo so', 20);
+
 ?>
