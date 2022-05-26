964ed16cc9a7c36a2226797a3e5a681efbfaf5b5
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-26 22:24:38 +0200

    Provato ancora un tentativo coi methods ma ancora fermo

--- Models/User.php
+++ Models/User.php
@@ -1,21 +1,29 @@
 <?php 
 
 class User extends Users {
-/* Avvio nuove caratteristiche */
-    public $id_user;
+    /* Nuove caratteristiche */
+    public $username; // il nome dell'utente --> dovrebbe essere restricted
+    public $password; // la password dell'utente --> dovrebbe essere restricted
 
     /* Qui il costrutto della classe User */
-    public function __construct(String $nome, String $cognome, Int $eta, String $indirizzo, String $email, String $payment, Int $id_user ) {
-    parent::__construct($nome, $cognome, $eta, $indirizzo, $email, $payment); // Nel figlio NON ci può essere string
-    $this->id_user = $id_user; // Questa è l'istanza di una variabile presente solo in User
+    public function __construct(String $nome, String $cognome, Int $eta, String $indirizzo, String $email, String $payment, Int $sales, Bool $id_user, String $username, String $password ) {
+    parent::__construct($nome, $cognome, $eta, $indirizzo, $email, $payment, $sales, $id_user); // Nel figlio NON ci può essere string
+        $this->username = $username; // Questa è l'istanza per l'username
+        $this->password = $password; // Questa è l'istanza per l'username
     }   
     /* Avvio stesso methods */
     public function parla() {
         return "Io Parlo da User";
     }
+
+    /* Avvio method */
+    public function setIdUser(){
+        $this -> id_user = true;
+    }
+    /* Avvio method per  */
+    public function getIdUser(){
+        return $this -> id_user;
+    }
 }
-
-
-
 
 ?>
