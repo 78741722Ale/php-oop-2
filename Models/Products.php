c63b05a75d6ae70fefb1ec581d71687143d3c469
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-29 11:23:45 +0200

    Risolto bug dello sconto eliminando i return superflui

--- Models/Products.php
+++ Models/Products.php
@@ -7,40 +7,27 @@ class Products {
     public $nome; // Nome Prodotto
     public $type; // Tipologia Prodotto
     public $desc; // Descrizione prodotto
-    public $price; // Prezzo
     public $utility; // Utilitizzo del prodotto
 
-    /* Avvio un method */
-    public function getSpecialPrice($price, $sale){
-        return $this->price = $price; // essendo protetto $famiglia lo salvo in una funzione pubblica
-        return $this->sale = $sale;
-    }
     /* Avvio altro method per valutare ereditarietà */
     public function parla() {
         return "Io Parlo da Products";
     }
-    /* Avvio altro method per recuperare il prezzo e spostarlo in User */
-    public function recupero(User $sales, User $id_user) {
-        $this->sales = $sales;
-        $this->id_user = $id_user;
-    }
-
-
     /* Definisco un construct */
-    function __construct(String $nome, String $type, String $desc, Int $price, String $utility) {
+    function __construct(String $nome, String $type, String $desc, String $utility) {
         /* Definisco le istanze all'interno del construtto */
         $this->nome = $nome;
         $this->type = $type;
         $this->desc = $desc;
-        $this->price = $price;
         $this->utility = $utility;
     }
 } 
 /* Avvio verifiche delle istanze */
-$istanza_products = new Products("Pallina", "Oggetto Inanimato", "Oggetto inanimato senza utilizzo", 100, "Non ha utilizzo");
+$istanza_products = new Products("Pallina", "Oggetto Inanimato", "Oggetto inanimato senza utilizzo", "Non ha utilizzo");
 var_dump("Questo è il var dump dell'istanza presente solo in products");
 var_dump($istanza_products);
 var_dump($istanza_products->setSaySomething("Questo è un trait presente solo in Products.php"));
+var_dump("Il prezzo scontato equivale a : " . $istanza_products->setSales(100, 500, 0.2));
 
 
 ?>
--- Models/User.php
+++ Models/User.php
@@ -2,10 +2,32 @@
 
 /* Avvio di un trait */
 trait userTrait {
-    public $string;
+    public $string; // Stringa del trait per verificarne l'effettivo utilizzo
+    public $id_user; // id user, nella condizione set/get
+    public $sales; // sconto se registrato
     /* Function per verificare funzionamento del trait */
     public function setSaySomething(String $string) {
         return $this->string = $string;
+    }
+
+    /* Funzione Pubblica per ricavare lo sconto in base all'utente */
+    /* Dovrei spostarla in products dato che di la c'è il prezzo */
+    /*  Qui non ha senso utiliizzare il prezzo */
+    public function setSales(Int $price, Int $id_user, float $sales) {
+        $this->price = $price;
+        $this->id_user = $id_user;
+        $this->sales = $sales;
+        
+        if($this->id_user == 0) 
+        {   /* Se l'id utente è uguale a 0 */
+            var_dump("L'utente dev'essere registrato per ottenere lo sconto del 20%");
+            return $this->price;
+        } 
+        elseif($this->id_user != 0) 
+        {   /* Altrimenti applica lo sconto */
+            return $this->price * $this->sales;
+        }
+
     }
 }
 
@@ -19,50 +41,26 @@ class User {
     public $indirizzo; // Indirizzo utente 
     public $email; // indirizzo email
     public $payment; // pagamenti
-    public $price; // QUESTA NON ANDREBBE QUA
-    public $sales; // sconto se registrato
-    public $id_user; // id user, nella condizione set/get
     protected $username; // il nome dell'utente --> dovrebbe essere protected
     protected $password; // la password dell'utente --> dovrebbe essere restricted
 
     /* Qui il costrutto della classe User */
-    public function __construct(String $nome, String $cognome, Int $eta, String $indirizzo, String $email, String $payment, float $price, float $sales, Int $id_user, String $username, String $password ) {
+    public function __construct(String $nome, String $cognome, Int $eta, String $indirizzo, String $email, String $payment,  String $username, String $password ) {
         $this->nome = $nome;
         $this->cognome = $cognome;
         $this->eta = $eta;
         $this->indirizzo = $indirizzo;
         $this->email = $email;
         $this->payment = $payment;
-        $this->price = $price; // QUESTA NON ANDREBBE QUA
-        $this->sales = $sales;
-        $this->id_user = $id_user;
         $this->username = $username; 
         $this->password = $password;
     }   
-
-    /* Funzione Pubblica per ricavare lo sconto in base all'utente */
-    /* Dovrei spostarla in products dato che di la c'è il prezzo */
-    /*  Qui non ha senso utiliizzare il prezzo */
-    public function setSales($price, $id_user, $sales) {
-        if($this->id_user == 0) 
-        {   /* Se l'id utente è uguale a 0 */
-            var_dump("L'utente dev'essere registrato per ottenere lo sconto del 20%");
-            return $this->price;
-        } 
-        elseif($this->id_user != 0) 
-        {   /* Altrimenti applica lo sconto */
-            return $this->price * $this->sales;
-        }
-    }
-
 }
 
 /* Avvio dell'istanza di User */
-$istanza_user = new User('Alessandro', 'Pecorilla', 27, 'Via di qua', 'prova@prova.com', 'Bancomat', 100, 0.2, 555, 'Alessandro', 'Password');
+$istanza_user = new User('Alessandro', 'Pecorilla', 27, 'Via di qua', 'prova@prova.com', 'Bancomat', 'Alessandro', 'Password');
 var_dump("Questa è l'istanza di User");
 var_dump($istanza_user);
-/* Funziona, se non fosse per il fatto che il Prezzo non può stare qua dentro */
-var_dump("Il prezzo scontato equivale a : " . $istanza_user->setSales(100, 555, 0.2));
 /* Qui il trait funziona */
 var_dump($istanza_user->setSaySomething("Questo è un trait presente solo in User.php"));
 
